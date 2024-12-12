16e3cc18acb41c519c93c35043b310c827fe4811
thanaphat-tim12 <thnphathrtasay044@gmail.com>
2024-12-12 13:20:27 +0700

    first commit

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ChirpController extends Controller
+{
+
+
+    public function index(): Response
+    {
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
+        ]);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+
+    public function store(Request $request): RedirectResponse
+    {
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Chirp $chirp): RedirectResponse
+    {
+        Gate::authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    public function destroy(Chirp $chirp): RedirectResponse
+    {
+        Gate::authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
+    }
+
+}
